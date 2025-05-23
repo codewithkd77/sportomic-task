7caac577689ff8697f7476884749d6b138aa55ae
KULDEEPSINH <rajputkuldeep59387@gmail.com>
2025-05-23 22:46:48 +0530

    origin issue resolved

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // Update CORS configuration to allow your client domain
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://sportomic-task-wgco.vercel.app'],
+  origin: ['http://localhost:3000', 'https://sportomic-task-2.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
